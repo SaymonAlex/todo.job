a68484ba9d7021f0f000fec2ed59215c2138e40b
SaymonAlex <109030346+SaymonAlex@users.noreply.github.com>
2026-01-17 17:55:31 +0200

    commit 1

---
+++ app.js
@@ -0,0 +1,167 @@
+// --- Кнопка для обновления ---
+const update_app = document.getElementById('update_app');
+update_app.style.display = 'none'; // скрыта по умолчанию
+
+// --- Service Worker регистрация ---
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(reg => {
+    // Слушаем появление новой версии SW
+    reg.onupdatefound = () => {
+      const newWorker = reg.installing;
+      newWorker.onstatechange = () => {
+        if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+          // Новый контент доступен
+          update_app.style.display = 'flex';
+        }
+      };
+    };
+  }).catch(console.error);
+}
+
+// При клике на кнопку обновления
+update_app.onclick = () => {
+  window.location.reload(); // перезагрузка страницы и активация нового SW
+  speak("Приложение обновлено");
+};
+
+// --- Тут твой код  ---
+
+let sound_voice = true;
+let utterance = null;
+
+function speak(text) {
+  if (!sound_voice) return;
+  speechSynthesis.cancel();
+  utterance = new SpeechSynthesisUtterance(text);
+  speechSynthesis.speak(utterance);
+}
+
+// Находим элементы на странице
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList');
+
+let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+	tasks = JSON.parse(localStorage.getItem('tasks'));
+	tasks.forEach((task) => renderTask(task));
+}
+
+checkEmptyList();
+
+form.addEventListener('submit', addTask);
+tasksList.addEventListener('click', deleteTask);
+tasksList.addEventListener('click', doneTask);
+
+// Функции
+function addTask(event) {
+	// Отменяем отправку формы
+	event.preventDefault();
+
+	// Достаем текст задачи из поля ввода
+	const taskText = taskInput.value;
+
+	// Описываем задачу в виде объекта
+	const newTask = {
+		id: Date.now(),
+		text: taskText,
+		done: false,
+	};
+
+	// Добавляем задачу в массив с задачами
+	tasks.push(newTask);
+
+	// Сохраняем список задач в хранилище браузера localStorage
+	saveToLocalStorage();
+
+	// Рендерим задачу на странице
+	renderTask(newTask);
+
+	// Очищаем поле ввода и возвращаем на него фокус
+	taskInput.value = '';
+	taskInput.focus();
+
+	checkEmptyList();
+}
+
+function deleteTask(event) {
+	// Проверяем если клик был НЕ по кнопке "удалить задачу"
+	if (event.target.dataset.action !== 'delete') return;
+
+	const parenNode = event.target.closest('.list-group-item');
+
+	// Определяем ID задачи
+	const id = Number(parenNode.id);
+
+	// Удаляем задча через фильтрацию массива
+	tasks = tasks.filter((task) => task.id !== id);
+
+	// Сохраняем список задач в хранилище браузера localStorage
+	saveToLocalStorage();
+
+	// Удаляем задачу из разметки
+	parenNode.remove();
+
+	checkEmptyList();
+}
+
+function doneTask(event) {
+	// Проверяем что клик был НЕ по кнопке "задача выполнена"
+	if (event.target.dataset.action !== 'done') return;
+
+	const parentNode = event.target.closest('.list-group-item');
+
+	// Определяем ID задачи
+	const id = Number(parentNode.id);
+	const task = tasks.find((task) => task.id === id);
+	task.done = !task.done;
+
+	// Сохраняем список задач в хранилище браузера localStorage
+	saveToLocalStorage();
+
+	const taskTitle = parentNode.querySelector('.task-title');
+	taskTitle.classList.toggle('task-title--done');
+}
+
+function checkEmptyList() {
+	if (tasks.length === 0) {
+		const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+					<img src="./img/list.png" alt="Empty" width="52" class="mt-3">
+					<div class="empty-list__title">Список задач пуст</div>
+				</li>`;
+		tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+	}
+
+	if (tasks.length > 0) {
+		const emptyListEl = document.querySelector('#emptyList');
+		emptyListEl ? emptyListEl.remove() : null;
+	}
+}
+
+function saveToLocalStorage() {
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function renderTask(task) {
+	// Формируем CSS класс
+	const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+
+	// Формируем разметку для новой задачи
+	const taskHTML = `
+                <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+					<span class="${cssClass}">${task.text}</span>
+					<div class="task-item__buttons">
+						<button type="button" data-action="done" class="btn-action">
+							<img src="./img/tick.svg" alt="Done" width="18" height="18">
+						</button>
+						<button type="button" data-action="delete" class="btn-action">
+							<img src="./img/cross.svg" alt="Done" width="18" height="18">
+						</button>
+					</div>
+				</li>`;
+
+	// Добавляем задачу на страницу
+	tasksList.insertAdjacentHTML('beforeend', taskHTML);
+}
